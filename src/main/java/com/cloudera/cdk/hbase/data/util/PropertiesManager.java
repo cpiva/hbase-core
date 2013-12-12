2bf256b50e10e433a1fd5639374a8cbddeebf3f2
svallely <svallely@cloudera.com>
2013-12-12 01:40:19 -0800

    externalize hbase url property

---
+++ src/main/java/com/cloudera/cdk/hbase/data/util/PropertiesManager.java
@@ -0,0 +1,26 @@
+package com.cloudera.cdk.hbase.data.util;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import org.apache.log4j.Logger;
+
+public class PropertiesManager {
+
+	private static Logger log = Logger.getLogger(PropertiesManager.class);
+	
+	public static String getProperty(String propertyKey) throws IOException	{
+		String value = null;
+		log.debug("Attempting to load property " + propertyKey);
+		InputStream is = Thread.currentThread().getContextClassLoader().getResourceAsStream("ecrr.properties");
+		if (is != null) {
+			Properties p = new Properties();
+			p.load(is);
+			value = p.getProperty(propertyKey);
+			log.info("Successfully loaded property value " + value);
+		}
+		return value;
+	}		
+
+}
