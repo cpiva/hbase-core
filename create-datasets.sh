2e9a7f21ce8f22cb0137ebb665adb23ea882dc23
Kaufman Ng <kaufman@cloudera.com>
2013-12-18 18:49:49 -0500

    scripts now require repo uri

--- create-datasets.sh
+++ create-datasets.sh
@@ -1,19 +1,31 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+  echo "usage: $0 <hbase repositoryUri>"
+  echo "e.g.:"
+  echo "  $0 repo:hbase:zk"
+  echo "  $0 repo:hbase:zk1,zk2,zk3"  
+  exit 1
+fi
+
+echo "using repositoryUri: $1"
+
 mvn cdk:create-dataset \
-  -Dcdk.repositoryUri=repo:hbase:localhost.localdomain \
+  -Dcdk.repositoryUri=$1 \
   -Dcdk.datasetName=party \
   -Dcdk.avroSchemaFile=src/main/avro/party.avsc
 
 mvn cdk:create-dataset \
-  -Dcdk.repositoryUri=repo:hbase:localhost.localdomain \
+  -Dcdk.repositoryUri=$1 \
   -Dcdk.datasetName=event \
   -Dcdk.avroSchemaFile=src/main/avro/event.avsc
 
 mvn cdk:create-dataset \
-  -Dcdk.repositoryUri=repo:hbase:localhost.localdomain \
+  -Dcdk.repositoryUri=$1 \
   -Dcdk.datasetName=address \
   -Dcdk.avroSchemaFile=src/main/avro/address.avsc
 
 mvn cdk:create-dataset \
-  -Dcdk.repositoryUri=repo:hbase:localhost.localdomain \
+  -Dcdk.repositoryUri=$1 \
   -Dcdk.datasetName=party_address \
   -Dcdk.avroSchemaFile=src/main/avro/party_address.avsc  
--- delete-datasets.sh
+++ delete-datasets.sh
@@ -1,19 +1,27 @@
-mvn cdk:delete-dataset \
-  -Dcdk.repositoryUri=repo:hbase:localhost.localdomain \
-  -Dcdk.datasetName=party \
-  -Dcdk.avroSchemaFile=src/main/avro/party.avsc
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+  echo "usage: $0 <hbase repositoryUri>"
+  echo "e.g.:"
+  echo "  $0 repo:hbase:zk"
+  echo "  $0 repo:hbase:zk1,zk2,zk3"  
+  exit 1
+fi
+
+echo "using repositoryUri: $1"
 
 mvn cdk:delete-dataset \
-  -Dcdk.repositoryUri=repo:hbase:localhost.localdomain \
+  -Dcdk.repositoryUri=$1 \
+  -Dcdk.datasetName=party
+
+mvn cdk:delete-dataset \
+  -Dcdk.repositoryUri=$1 \
   -Dcdk.datasetName=event \
-  -Dcdk.avroSchemaFile=src/main/avro/event.avsc
 
 mvn cdk:delete-dataset \
-  -Dcdk.repositoryUri=repo:hbase:localhost.localdomain \
+  -Dcdk.repositoryUri=$1 \
   -Dcdk.datasetName=address \
-  -Dcdk.avroSchemaFile=src/main/avro/address.avsc
 
 mvn cdk:delete-dataset \
-  -Dcdk.repositoryUri=repo:hbase:localhost.localdomain \
+  -Dcdk.repositoryUri=$1 \
   -Dcdk.datasetName=party_address \
-  -Dcdk.avroSchemaFile=src/main/avro/party_address.avsc    
